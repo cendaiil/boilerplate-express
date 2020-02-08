0452600c6aba24e7fc15d00126f42bd2827148fc
Angelo Carrara <angelo.carrara@enginium.eu>
2020-02-08 11:51:27 +0100

    final version

--- myApp.js
+++ myApp.js
@@ -1,13 +1,19 @@
 
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 // --> 7)  Mount the Logger middleware here
-
+let logInfo = function (req, res, next){
+    console.log(req.method + " " + req.path + " - " + req.ip)
+    next()
+}
+app.use(logInfo)
 
 
 // --> 11)  Mount the body-parser middleware  here
-
+let urlEncoded = bodyParser.urlencoded({extended: false})
+app.use(urlEncoded)
 
 /** 1) Meet the node console. */
 
@@ -21,13 +27,7 @@ app.get("/", function (req, res) {
     absolutePath = __dirname + "/views/index.html"
     res.sendFile(absolutePath);
 })
-/** 7) Root-level Middleware - A logger */
-//  place it before all the routes !
-let logInfo = function (req, res, next){
-    console.log(req.method + " " + req.path + " - " + req.ip)
-    next()
-}
-app.use(logInfo)
+
 /** 4) Serve static assets  */
 app.use(express.static(__dirname + "/public"))
 
@@ -47,23 +47,39 @@ let sendJson = function (req, res) {
 app.get("/json", sendJson)
 
 
+/** 7) Root-level Middleware - A logger */
+//  place it before all the routes !
+
 
 /** 8) Chaining middleware. A Time server */
-
+app.get('/now', function (req, res, next){
+ req.time = new Date().toString();
+ next()   
+}, function(req, res){
+    res.send({"time": req.time})
+})
 
 /** 9)  Get input from client - Route parameters */
-
+app.get("/:word/echo",function(req, res){
+    res.send({"echo": req.params.word})
+})
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
-
+// app.get('/name', function(req, res){
+//     res.send({name: req.query.first+" " + req.query.last})
+// })
 
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
 
 
 /** 12) Get data form POST  */
-
+app.route('/name').get(function(req, res){
+    res.send({name: req.query.first+" " + req.query.last})
+}).post(function(req, res){
+    res.send({name: req.body.first+" " + req.body.last})
+})
 
 
 // This would be part of the basic setup of an Express app
